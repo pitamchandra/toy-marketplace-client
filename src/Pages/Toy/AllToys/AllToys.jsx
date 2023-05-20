e511ac8ae462b38f08e01f56944fcd7b7cb86c1a
pitamchandra <pitamchandra402@gmail.com>
2023-05-20 20:08:08 +0600

    search functionality added in the all toy pages

--- src/Pages/Toy/AllToys/AllToys.jsx
+++ src/Pages/Toy/AllToys/AllToys.jsx
@@ -1,17 +1,29 @@
 import { useLoaderData } from "react-router-dom";
 import ToyRow from "./ToyRow";
+import { useState } from "react";
 
 
 const AllToys = () => {
 
-    const allToys = useLoaderData()
+    const toys = useLoaderData()
+    const [allToys, setAllToys] = useState(toys)
+
+    const [searchText, setSearchText] = useState('')
+    console.log(searchText);
+
+    const handleSearch = () =>{
+
+        const searchToys = toys.filter(toy => toy.name == searchText)
+        setAllToys(searchToys);
+
+    }
 
     return (
         <div className="container mx-auto px-3 md:px-0 my-10">
             <h2 className="text-2xl font-bold mb-6">All Toys : {allToys.length}</h2>
             <div className="md:w-2/4 mx-auto relative mb-10">
-                <input type="text" name="" placeholder="search" className="border border-gray-500 w-full p-3"/>
-                <button className="btn btn-error absolute top-0 right-0 rounded-none text-white px-7">search</button>
+                <input type="text" name="" onChange={(e) => setSearchText(e.target.value)} placeholder="search" className="border border-gray-500 w-full p-3"/>{" "}
+                <button onClick={handleSearch} className="btn btn-error absolute top-0 right-0 rounded-none text-white px-7">search</button>
             </div>
             <div className="overflow-x-auto w-full">
                 <table className="table text-center w-full">
@@ -28,7 +40,7 @@ const AllToys = () => {
                     </thead>
                     <tbody>
                     {
-                        allToys.map(toy => <ToyRow key={toy._id} toy={toy}></ToyRow>)
+                        allToys?.slice(0, 20)?.map(toy => <ToyRow key={toy._id} toy={toy}></ToyRow>)
                     }
                     </tbody>
                 </table>
